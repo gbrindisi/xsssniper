89b7210124039f197a55df7979be1a6a23624fa3
Gianluca Brindisi <g@brindi.si>
2011-09-28 14:46:58 +0200

    Refactored the scan engine

--- core/result.py
+++ core/result.py
@@ -3,31 +3,16 @@ class Result:
     A Result represent a successful XSS injection.
     Used to clean the Scanner and handle printing
     """
-    def __init__(self, url, param, method, payload, mode):
-        """
-        Mode: 0 normal check
-              1 uppercase check
-              2 lowercase check
-        """
+    def __init__(self, url, param, method, taint, injectiontype):
         self.url = url
         self.param = param
         self.method = method
-        self.payload = payload
-        if mode is 1:
-            self.mode = mode
-            self.check = self.payload.check.uppercase()
-        elif mode is 2:
-            self.mode = mode
-            self.check = self.payload.check.lowercase()
-        else:
-            self.mode = 0
-            self.check = self.payload.check
+        self.taint = taint
+        self.injectiontype = injectiontype
 
     def printResult(self):
         print "\n[!] XSS Found:\t%s" % self.url
         print "    Method:\t%s" % self.method
         print "    Param:\t%s" % self.param
-        print "    Payload:\t%s" % self.payload.payload
-        print "    Check:\t%s" % self.check
-        print "    Desc:\t%s" % self.payload.description
-        print "    Reference:\t%s" % self.payload.reference
+        print "    Taint:\t%s" % self.taint
+        print "    Injection Type:\t%s" % self.injectiontype
--- core/scanner.py
+++ core/scanner.py
@@ -18,6 +18,8 @@ import re
 import Queue
 import threading
 import time
+import random
+import string
 
 from core.target import Target
 from core.payload import Payload
@@ -176,60 +178,234 @@ class ScannerThread(threading.Thread):
         self.queue = queue
         self.scannerengine = scannerengine
 
+    def processResponse(self, response, seed):
+        """
+        How it works: we parse the response sequentially
+        looking for the seed while keeping
+        a state of the current position to determine if we have
+        a valid injection and where.
+
+        This is based on ratproxy XSS scanning technique so
+        all the props to @lcamtuf for this.
+        """
+        htmlstate = 0
+        htmlurl = 0
+        index = 0
+
+        """
+        An array of xss identifiers
+        they are int... needs improvement
+        """
+        result = []
+
+        """ 
+        Building the taint and the response
+        I want everything lowercase because I don't want to handle 
+        cases when the payload is upper/lowercased by the webserver
+        """
+        seed_len = len(seed)
+        taint = "{0}:{0} {0}=-->{0}\"{0}>{0}'{0}>{0}+{0}<{0}>".format(seed)
+        response = response.read().lower()
+
+        """
+        Now start the scanning
+        htmlstate legend:
+        - 1 index is in tag
+        - 2 index is inside double quotes
+        - 4 index is inside single quotes
+        - 8 index is inside html comment
+        - 16 index is inside cdata
+        """
+        while index <= len(response)-1:
+            """
+            Exit cases for a match against the taint
+            If conditions are a little messy...
+            TODO: utf-7 xss
+            """
+            if response[index:index+seed_len] == seed:
+                """
+                XSS found in tag
+                <tag foo=bar onload=...>
+                type 1
+                """ 
+                if htmlstate == 1 and response[index+seed_len:index+seed_len+seed_len+1] == " " + seed + "=":
+                    index = index + seed_len
+                    result.append(1)
+                    continue
+
+                """
+                XSS found in url
+                <tag src=foo:bar ...>
+                type 2
+                """
+                if htmlurl and response[index+seed_len:index+seed_len+seed_len+1] == ":" + seed:
+                    index = index + seed_len
+                    result.append(2)
+                    continue
+
+                """
+                XSS found freely in response
+                <tag><script>...
+                type 3
+                """
+                if htmlstate == 0 and response[index+seed_len:index+seed_len+seed_len+1] == "<" + seed:
+                    index  = index + seed_len
+                    result.append(3)
+                    continue
+
+                """
+                XSS found inside double quotes
+                <tag foo="bar"onload=...>
+                type 4
+                """
+                if (htmlstate == 1 or htmlstate == 2) and response[index+seed_len:index+seed_len+seed_len] == "\"" + seed:
+                    index = index + seed_len
+                    result.append(4)
+                    continue
+
+                """
+                XSS found inside single quotes
+                <tag foo='bar'onload=...>
+                """
+                if (htmlstate == 1 or htmlstate == 4) and response[index+seed_len:index+seed_len+seed_len] == "'" + seed:
+                    index  = index + seed_len
+                    result.append(5)
+                    continue
+
+            else:
+                """
+                We are in a CDATA block
+                """
+                if (htmlstate == 0 and response[index:index+9] == "<![CDATA["):
+                    htmlstate = 16
+                    index = index + 9
+                    continue
+                if htmlstate == 16 and response[index:index+3] == "]]>":
+                    htmlstate = 0
+                    index = index + 3
+                    continue
+
+                """
+                We are in a html comment
+                """
+                if htmlstate == 0 and response[index:index+4] == "<!--":
+                    htmlstate = 8
+                    index = index + 4
+                    continue
+                if htmlstate == 8 and response[index:index+3] == "-->":
+                    htmlstate = 0
+                    index = index + 3
+                    continue
+
+                """
+                We are in a tag
+                """
+                if htmlstate == 0 and response[index] == "<" and (response[index+1] == "!" or response[index+1] == "?" or response[index+1].isalpha()):
+                    htmlstate = 1
+                    index = index + 1
+                    continue
+                if htmlstate == 1 and response[index] == ">":
+                    htmlstate = 0
+                    htmlurl = 0
+                    index = index + 1
+                    continue
+
+                """
+                We are inside a double quote
+                """
+                if htmlstate == 1 and response[index] == '"' and response[index-1] == '=':
+                    htmlstate = 2
+                    index = index + 1
+                    continue
+                if (htmlstate == 1 or htmlstate == 2) and response[index] == '"':
+                    htmlstate = 1
+                    index = index + 1
+                    continue
+
+                """
+                We are inside a single quote
+                """
+                if htmlstate == 1 and response[index] == '\'' and response[index-1] == '=':
+                    htmlstate = 4
+                    index = index + 1
+                    continue
+
+                if (htmlstate == 1 or htmlstate == 4) and response[index] == '\'':
+                    htmlstate = 1
+                    index = index + 1
+                    continue
+
+                """
+                We are inside an url
+                """
+                if htmlstate == 1 and response[index-1] == " " and response[index:index+5] == "href=":
+                    htmlurl = 1
+                    index = index + 5 
+                    continue
+                if htmlstate == 1 and response[index-1] == " " and response[index:index+5] == "src=":
+                    htmlurl = 1
+                    index = index + 4
+                    continue
+
+                """
+                In case the url isn't correctly closed
+                """
+                if htmlurl == 1: htmlurl = 0
+
+            """ Move on """
+            index = index +1
+
+        """ End of response parsing """
+        return result
+
+
+
+            
     def run(self):
         while True:
             try:
                 target = self.queue.get(block=False)
             
-            # If queue is empty or whatever
+            """ If queue is empty or whatever """
             except:
                 try:
                     self.queue.task_done()
                 except ValueError:
-                    # Can't handle this
+                    """ Can't handle this """
                     pass
 
-            # Otherwise...
+            """ Otherwise... """
             else:
-                # No GET/POST parameters? Skip to next url
+                """ No GET/POST parameters? Skip to next url """
                 if len(target.params) == 0:
                     print "[X] No paramaters to inject"
                     self.queue.task_done()
                     continue
 
-                # Check every parameter
+                """ Check every parameter """
                 for k, v in target.params.iteritems():
-                    for pl in self.scannerengine.payloads:
-                        url, data = target.getPayloadedUrl(k, pl.payload)
-                        if self.scannerengine.getOption('http-proxy') is not None:
-                            proxy = ProxyHandler({'http': self.getOption('http-proxy')})
-                            opener = build_opener(proxy)
-                            install_opener(opener)
-                        req = Request(url, data)
-                        # TODO: A verbose option, for now print only when you find something
-                        # print "[-] Testing:\t%s" % pl.getPayload()
-                        # print "    Param:\t%s" % k
-                        # print "    Url:\t%s" % url
-                        try: response = urlopen(req)
-                        except HTTPError, e:
-                            print "[X] Error: %s on %s" % (e.code, url)
-                            continue
-                        except URLError, e:
-                            print "[X] Error: can't connect"
-                            continue
-                        else:
-                            result = response.read()
-                            if result.find(pl.check) != -1:
-                                r = Result(url, k, target.method, pl, 0)
-                                self.scannerengine.addResult(r)
-                            elif result.find(pl.check.lower()) != -1:
-                                r = Result(url, k, target.method, pl, 2)
-                                self.scannerengine.addResult(r)
-                            elif result.find(pl.check.upper()) != -1:
-                                r = Result(url, k, target.method, pl, 1)
-                                self.scannerengine.addResult(r)
-                            else:
-                                pass
+                    seed_len = 4 #TODO: control over this
+                    seed = ''.join(random.choice(string.ascii_letters + string.digits) for x in range(seed_len)).lower()
+                    taint = "{0}:{0} {0}=-->{0}\"{0}>{0}'{0}>{0}+{0}<{0}>".format(seed)
+                    url, data = target.getPayloadedUrl(k, taint)
+                    """ In case of proxy """
+                    if self.scannerengine.getOption('http-proxy') is not None:
+                        proxy = ProxyHandler({'http': self.getOption('http-proxy')})
+                        opener = build_opener(proxy)
+                        install_opener(opener)
+                    """ Build the request """
+                    req = Request(url, data)
+                    try: response = urlopen(req)
+                    except HTTPError, e:
+                        print "[X] Error: %s on %s" % (e.code, url)
+                        continue
+                    except URLError, e:
+                        print "[X] Error: can't connect"
+                        continue
+                    else:
+                        result = self.processResponse(response, seed)
+                        for r in result:
+                            self.scannerengine.addResult(Result(url, k, target.method, taint, r))
                 
                 # Scan complete                
                 self.queue.task_done()
